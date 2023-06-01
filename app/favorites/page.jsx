789b8ae832cb566cdc5752500f461a3c620fb38d
SziNo <szino1848@gmail.com>
2023-06-02 00:01:02 +0200

    make Favorites component route protected

--- app/favorites/page.jsx
+++ app/favorites/page.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session?.user) {
+      setTimeout(() => {
+        router.push('/');
+      }, 4000);
+    }
+  }, [session, router]);
+
+  return (
+    <div>
+      {!session?.user ? (
+        <h1 className="desc">You need to log in to access this page!</h1>
+      ) : (
+        <>
+          <h1>Favorites</h1>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Favorites;
