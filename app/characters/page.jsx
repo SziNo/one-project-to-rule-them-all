c2e56a99cd9c9e65b6ca0ee8dfe1c6d7eb9e4139
SziNo <szino1848@gmail.com>
2023-06-06 00:03:31 +0200

    create Characters component

--- app/characters/page.jsx
+++ app/characters/page.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+'use client';
+
+import ListComponent from '@components/ListComponent';
 
 const Characters = () => {
-  return <div>Characters</div>;
+  return (
+    <section className="flex flex-col justify-center items-center w-full p-5">
+      <h1 className="orange_gradient text-center mb-2 font-bold text-2xl md:text-5xl">
+        Characters
+      </h1>
+
+      <ListComponent />
+    </section>
+  );
 };
 
 export default Characters;
