371ceae76bd13642135fd62554218ba6c32cab8f
SziNo <szino1848@gmail.com>
2023-06-06 00:03:16 +0200

    create Books component

--- app/books/page.jsx
+++ app/books/page.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+'use client';
+
+import ListComponent from '@components/ListComponent';
 
 const Books = () => {
-  return <div>Books</div>;
+  return (
+    <section className="flex flex-col justify-center items-center w-full p-5">
+      <h1 className="orange_gradient text-center mb-2 font-bold text-2xl md:text-5xl">
+        Books
+      </h1>
+
+      <ListComponent />
+    </section>
+  );
 };
 
 export default Books;
+
+// name: 'The Fellowship Of The Ring';
+// _id: '5cf5805fb53e011a64671582';
