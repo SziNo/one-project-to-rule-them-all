4ad3b60e13f75b0a1fc157bb06288efa09ef01c7
SziNo <szino1848@gmail.com>
2023-06-07 00:05:03 +0200

    update navLinks

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 export const THE_ONE_API_BASE_URL = 'https://the-one-api.dev/v2';
 
 export const navLinks = [
-  { href: '/books', title: 'Books' },
-  { href: '/movies', title: 'Movies' },
-  { href: '/characters', title: 'Characters' },
+  { href: '/book', title: 'Books' },
+  { href: '/movie', title: 'Movies' },
+  { href: '/character', title: 'Characters' },
 ];
