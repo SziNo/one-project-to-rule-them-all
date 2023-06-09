3dc6a7d70434509fa5a39dc3b0e5842a545172cf
SziNo <szino1848@gmail.com>
2023-06-10 00:05:21 +0200

    update character deatil page

--- app/character/[id]/page.jsx
+++ app/character/[id]/page.jsx
@@ -1,15 +1,47 @@
 import { getLOTRData } from '@utils/axios';
+import Image from 'next/image';
 
 const CharacterDetailPage = async ({ params }) => {
   const { docs: character } = await getLOTRData('character', params.id);
 
-  console.log(character[0]);
+  const { name, race, gender, birth, death, spouse } = character[0];
 
   return (
-    <div>
-      <h1>Character</h1>
-      <p>{params.id}</p>
-    </div>
+    <section className="flex-center flex-col w-full p-5">
+      <div className="flex-center flex-col gap-5">
+        <h2 className="text-4xl md:text-6xl text-gray-600 md:mb-20 mb-10 font-bold">
+          {name}
+        </h2>
+        <div className="flex-center gap-5 p-5">
+          <div className="flex flex-col gap-3 justify-start items-start p-5">
+            <p className="text-xl md:text-2xl italic text-gray-800 font-bold">
+              Race: <span className="text-amber-900 ">{race || 'N/A'}</span>
+            </p>
+            <p className="text-xl md:text-2xl italic text-gray-800 font-bold">
+              Gender: <span className="text-amber-900 ">{gender || 'N/A'}</span>
+            </p>
+            <p className="text-xl md:text-2xl italic text-gray-800 font-bold">
+              Birth: <span className="text-amber-900 ">{birth || 'N/A'}</span>
+            </p>
+            <p className="text-xl md:text-2xl italic text-gray-800 font-bold">
+              Death: <span className="text-amber-900 ">{death || 'N/A'}</span>
+            </p>
+            <p className="text-xl md:text-2xl italic text-gray-800 font-bold">
+              Spouse: <span className="text-amber-900 ">{spouse || 'N/A'}</span>
+            </p>
+          </div>
+
+          <div className="md:flex hidden p-5">
+            <Image
+              src="/assets/images/gimli.png"
+              alt="Gimli"
+              width={180}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
