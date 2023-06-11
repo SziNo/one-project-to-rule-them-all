fd7e53d891b70a8ad414de4d639ec677c79292b8
SziNo <szino1848@gmail.com>
2023-06-12 00:04:44 +0200

    add an extra short paragraph

--- app/character/page.jsx
+++ app/character/page.jsx
@@ -8,6 +8,9 @@ const Characters = () => {
       <h1 className="orange_gradient text-center mb-2 font-bold text-2xl md:text-5xl">
         Characters
       </h1>
+      <p className="mt-2 text-sm text-gray-700">
+        Log in and save your favorite characters!
+      </p>
 
       <ListComponent />
     </section>
