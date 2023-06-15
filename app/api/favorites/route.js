c7695027b201f68349dbaaae0b43416852710da3
SziNo <szino1848@gmail.com>
2023-06-15 23:47:44 +0200

    import parse to help extract userId from request, this fixed the issue

--- app/api/favorites/route.js
+++ app/api/favorites/route.js
@@ -1,13 +1,20 @@
 import Favorite from '@models/favorite';
 import { connectToDB } from '@utils/database';
+import { parse } from 'url';
 
 export const GET = async (request) => {
+  const {
+    query: { userId },
+  } = parse(request.url, true);
+
   try {
     await connectToDB();
-    const favorites = await Favorite.find({}).populate('user');
+    const favorites = await Favorite.find({ user: userId }).populate('user');
 
     return new Response(JSON.stringify(favorites), { status: 200 });
   } catch (error) {
+    // Log the error to see what's causing the issue
+    console.error('Error fetching favorites:', error);
     return new Response('Failed to fetch favorites', { status: 500 });
   }
 };
